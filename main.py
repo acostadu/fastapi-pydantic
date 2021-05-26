f80b23a63793e55394bb9b80efd05c9c7f2b8104
Danny Acosta <danny.acosta@walmart.com>
2021-05-26 10:45:10 -0400

    Added Pydantic and class Item

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
 
 
 @app.get("/")
@@ -9,5 +18,10 @@ def index():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str):
+def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/items/{item_id}")
+def save_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item": item.dict()}
